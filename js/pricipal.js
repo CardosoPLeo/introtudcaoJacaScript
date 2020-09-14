576bebf1b381bfa94b38047a8fe6bd91290c25ba
CardosoPLeo <leonardo_p_c@hotmail.com>
2020-09-13 22:24:25 -0300

    Implmentando calculo IMC para todos os elementos da tabela

--- js/pricipal.js
+++ js/pricipal.js
@@ -1,29 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso"); 
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++){
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i];
+    
+    var tdPeso = paciente.querySelector(".info-peso"); 
+    var peso = tdPeso.textContent;
 
-var tdImc = paciente.querySelector(".info-imc")
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
+    var tdImc = paciente.querySelector(".info-imc")
 
-if(peso <=0 || pesopeso >= 1000 ){
-    peso = false;
-    tdImc.textContent="Peso inválido!";
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if(peso <=0 || peso >= 1000 ){
+        peso = false;
+        tdImc.textContent="Peso inválido!";
+    }
+    if(altura <=0 || altura >=3.00){
+        altura = false;
+        tdImc.textContent ="Altura inválida!";
+    }
+
+    if(pesoValido && alturaValida){
+        var ICM = peso / (altura * altura);
+        tdImc.textContent = ICM.toFixed(2);
+    }else {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
+    }
+
 }
-if(altura <=0 || altura >=3.00){
-    altura = false;
-    tdImc.textContent ="Altura iinválida!";
-}
 
-if(pesoValido && alturaValida){
-    var ICM = peso / (altura * altura);
-    tdImc.textContent = ICM;
-}
