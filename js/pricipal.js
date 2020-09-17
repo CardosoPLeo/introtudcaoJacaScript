6198dc6cffa5ab15c9f36b2fab80f8a4586105ba
CardosoPLeo <leonardo_p_c@hotmail.com>
2020-09-16 23:06:57 -0300

    criada funcoes para adcionar o paciente na tabela

--- js/pricipal.js
+++ js/pricipal.js
@@ -38,7 +38,33 @@ for(var i = 0; i < pacientes.length; i++){
     var btnAdiconarPaciente = querySelector("#adicionar-paciente");
     btnAdiconarPaciente.addEventListner("click", function(event){
         event.preventDefault();
-                
+       
+        var form = querySelector("#adcionarPaciente")
+        var nome = form.nome.value;
+        var peso = form.peso.value;
+        var altura = form.altura.value;
+        var gordura = form.gordura.value;
+
+        var pacienteTr = documetn.createElement("tr");
+
+        var nomeTd = document.createElement("td");
+        var pesoTd = document.createElement("td");
+        var alturaTd = document.createElement("td");
+        var gorduraTd = document.createElement("td");
+        //var imcTd = document.createElement("td");
+
+    
+        nomeTd.textContent = nome;
+        pesoTd.textContent = peso;
+        alturaTd.textContent = altura;
+        gorduraTd.textContent = gordura;
+        
+        pacienteTr.appendChild(nomeTd);
+        pacienteTr.appendChild(pesoTd);
+        pacienteTr.appendChild(alturaTd);
+        pacienteTr.appendChild(gorduraTd);
+
+        var tabela = document.querySelector("#tabela-pacientes");
+        tabela.appendChild(pacienteTr);
     });
 }
-
