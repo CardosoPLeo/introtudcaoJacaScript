148206e1ceea4d06fbdb26c7dacc2a1b238c12de
CardosoPLeo <leonardo_p_c@hotmail.com>
2020-10-15 18:57:14 -0300

    Criado uma rotina caso ocorra algum erro

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -2,16 +2,20 @@ var btnBuscarPacientes = document.querySelector("#buscar-pacientes");
 btnBuscarPacientes.addEventListener("click", function(){
 
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes111");
 
     xhr.addEventListener("load", function(){
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
         
-        pacientes.forEach(function(paciente){
-            adicionaPacienteNaTabela(paciente);
-        });
+        if(xhr.status == 200){
+                var resposta = xhr.responseText;
+                var pacientes = JSON.parse(resposta);
+                
+                pacientes.forEach(function(paciente){
+                adicionaPacienteNaTabela(paciente);
+            });
+        }else{
+            alert("Page not found(404). Algo de errado ocorreu ao buscar as informações.")
+        }
     });
-
     xhr.send();
 });
